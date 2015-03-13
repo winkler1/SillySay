9deb1b60406b87c07fb58f45aefc84e79b52d5d6
Jeff Winkler <winkler1@gmail.com>
2015-03-13 18:36:24 -0400

    Say something right away to spin up speech on iPad.

--- scripts/index.js
+++ scripts/index.js
@@ -7,4 +7,9 @@ var React = require('react'),
 var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 
+// Spin up the speech code so there's not a delay on iPad when first playing words.
+var utterance = new SpeechSynthesisUtterance();
+utterance.volume=0;
+speechSynthesis.speak(utterance);
+
 React.render(<App />, document.body);
